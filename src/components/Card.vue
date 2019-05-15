0de01273596df5797605ae90f9866100369d505f
yjj <45am.me@gmail.com>
2019-05-16 03:55:55 +0900

    Card Component test

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,20 @@
-<template></template>
+<template>
+<section class="component-card-box">
+    <div class="component-card-wrap">
+    </div>
+</section>
+</template>
+
 
 <script>
-export default {};
+export default {
+    name: 'Card',
+};
 </script>
 
+
 <style>
+/* card layout */
+.component-card-box {min-height:50px;border-radius:5px;background:#ddd;}
+.component-card-wrap {padding:10px;}
 </style>
